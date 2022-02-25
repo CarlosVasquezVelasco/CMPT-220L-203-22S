2e41a0e986fcce9853b235d8b5e3a2c0c937e7dc
CarlosVasquezVelasco <Carlos.Vasquez1@marist.edu>
2022-02-24 22:31:05 -0500

    Lab 5

--- JavaLabs/Lab 5/src/base/Main.java
+++ JavaLabs/Lab 5/src/base/Main.java
@@ -1,5 +1,4 @@
 package base;
-
 public class Main {
     /**
      * In our Assignment here we're running a little lemonade stand and we also sell pretzels. We need to keep track of
@@ -21,109 +20,72 @@ public class Main {
      * Pretzels are $2 for 1
      * Lemonade is $8 for 1
      */
+
+    // starting values needed
     static int lemonades_available = 43;
     static int pretzels_available = 60;
-    static double cash = 1500.0;
+    static int cash = 1500;
     static double tips = 0.0;
+
+
+    // sum of arrays after first hour
+    static int sumLemons;
+    static int sumPretzels;
+    static int sumTip_rec;
+    // formula to actively update inventory
+    static int netLemons = lemonades_available - sumLemons;
+    static int netPretzels = pretzels_available - sumPretzels;
 
 
     public static void main(String[] args) {
         //	Replace this with your dry inventory function!
-        wet_inventory();
+        dry_inventory();
+    }
+    // array for purchases of pretzels in first hour
+    static int pretzelsBought[] = {4,2,3,1,2,10,6,1,2,6,9};
+    // formula to find the sum of array above
+    static int summationP()
+    {
+        int sumPretzels = 0;
+        int i;
+        for (i = 0; i < pretzelsBought.length; i++)
+            sumPretzels +=  pretzelsBought[i];
+        return sumPretzels;
+    }
+    // array for the number of lemons used
+    static int lemonsUsed[]= {1, 3, 2, 6, 4, 8, 1, 7, 3, 2};
+    // formula to find the sum of array above
+    static int summationL(){
+        int sumLemons = 0;
+        int x;
+        for (x = 0; x < lemonsUsed.length; x++)
+            sumLemons += lemonsUsed[x];
+        return sumLemons;
+    }
+    // array for the number of tips received
+    static int tipsReceived[] = {4, 10,5,10,4,3};
+    // formula to find the sum of array above
+    static int summationT(){
+        int sumTip_rec = 0;
+        int y;
+        for (y = 0; y < tipsReceived.length; y++)
+            sumTip_rec += tipsReceived[y];
+        return sumTip_rec;
     }
 
     static void dry_inventory() {
         // Your DRY Solution goes here!
-    }
-
-    static void wet_inventory() {
-        /**
-         * We have 10 customers in the first hour! This is what they bought! When running this with your dry code you
-         * don't want to run this function because then the numbers in the end will be wrong!
-         */
-        //Customer 1
-        pretzels_available -= 4;
-        cash += 8;
-        lemonades_available -= 1;
-        cash += 8;
-        tips += 4;
-
-        //Customer 2
-        pretzels_available -= 2;
-        cash += 4;
-        lemonades_available -= 3;
-        cash += 24;
-
-        //Customer 3
-        pretzels_available -= 3;
-        cash += 6;
-
-        //Customer 4
-        pretzels_available -= 1;
-        cash += 2;
-        lemonades_available -= 2;
-        cash += 16;
-
-        //Customer 5
-        lemonades_available -= 6;
-        cash += 48;
-        tips += 10;
-
-        //Customer 6
-        lemonades_available -= 4;
-        cash += 32;
-        tips += 5;
-
-        //Customer 7
-        pretzels_available -= 2;
-        cash += 4;
-
-        //Customer 8
-        pretzels_available -= 10;
-        cash += 20;
-        lemonades_available -= 8;
-        cash += 64;
-        tips += 10;
+        // formulas used to find the remaining inventory after hour 1
+        int netLemons = lemonades_available - summationL();
+        int netPretzels = pretzels_available - summationP();
+        // total cash after first hour
+        int totalCashFlows = cash + (2*summationP()) + (8*summationL());
 
 
-        //Customer 9
-        pretzels_available -= 6;
-        cash += 12;
-
-        //Customer 10
-        lemonades_available -= 1;
-        cash += 8;
-
-        //Customer 11
-        pretzels_available -= 1;
-        cash += 2;
-
-        //Customer 12
-        lemonades_available -= 7;
-        cash += 56;
-        tips += 4;
-
-        //Customer 13
-        pretzels_available -= 2;
-        cash += 4;
-
-        //Customer 14
-        pretzels_available -= 6;
-        cash += 12;
-        lemonades_available -= 3;
-        cash += 24;
-
-        //Customer 15
-        pretzels_available -= 9;
-        cash += 18;
-        lemonades_available -= 2;
-        cash += 16;
-        tips += 3;
-
-        System.out.println("Results for the hour!");
-        System.out.println("Lemonades Inventory: " + lemonades_available);
-        System.out.println("Pretzels Inventory: " + pretzels_available);
-        System.out.println("Cash: " + cash);
-        System.out.println("Tips: " + tips);
+    System.out.println("Results for the hour!");
+    System.out.println("Lemon Inventory: " + netLemons);
+    System.out.println("Pretzel Inventory: " + netPretzels);
+    System.out.println("Cash: " + totalCashFlows);
+    System.out.println("Tips: " + summationT());
     }
 }
