719647f2f94c5875da1305e23a766324e0931773
CarlosVasquezVelasco <Carlos.Vasquez1@marist.edu>
2022-03-03 18:36:52 -0500

    Lab 6

--- JavaLabs/Lab 6/src/base/Main.java
+++ JavaLabs/Lab 6/src/base/Main.java
@@ -1,6 +1,12 @@
 package base;
-
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.PriorityQueue;
+import java.util.Stack;
 public class Main {
+
     /**
      * # Lab Six
      * ---
@@ -21,12 +27,62 @@ public class Main {
      * - Populate the data types
      * - Print out the Linear Data Types
      */
+
     public static void main(String[] args) {
         // Create an Array, Populate it, and Print it out
+        String[] soccerA = {"ManCity", "ManUtd", "Chelsea", "Liverpool", "Barcelona", "RealMadrid"};
+        System.out.println((Arrays.toString(soccerA)));
         // Create an ArrayList, Populate it, and Print it out
+        ArrayList<String> soccerTeams = new ArrayList<String>();
+        soccerTeams.add("ManCity");
+        soccerTeams.add("ManUtd");
+        soccerTeams.add("Chelsea");
+        soccerTeams.add("Liverpool");
+        soccerTeams.add("Barcelona");
+        soccerTeams.add("RealMadrid");
+        System.out.println(soccerTeams);
         // Create an LinkedList, Populate it, and Print it out
+        LinkedList<String> basketballTeams = new LinkedList<String>();
+        basketballTeams.add("Boston Celtics");
+        basketballTeams.add("New York Knicks");
+        basketballTeams.add("LA Lakers ");
+        basketballTeams.add("OKC Thunder");
+        basketballTeams.add("Miami Heat");
+        System.out.println(basketballTeams);
         // Create an Queue, Populate it, and Print it out
+        Queue<String> animals = new LinkedList<String>();
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Bird");
+        animals.add("Lizards");
+        animals.add("Rabbits");
+        System.out.println(animals);
         // Create an Stack, Populate it, and Print it out
+        Stack<String> colors = new Stack<String>();
+        colors.push("Red");
+        colors.push("Blue");
+        colors.push("Grey");
+        colors.push("Black");
+        System.out.println(colors);
+
         // Using the information in the Lab 6 Jupyter Notebook create an instance of plant here.
+        Main Plant = new Main(true, true, 15, 2, true);
+        System.out.println("Weight: " + Plant.weight);
+        System.out.println("Height: " + Plant.height);
+    }
+    boolean photosynthetic;
+    boolean hasChlorophyll;
+    Integer height;
+    Integer weight;
+    boolean hasRoots;
+    public Main(boolean photosynthetic, boolean hasChlorophyll, Integer height, Integer weight, boolean hasRoots){
+        this.photosynthetic = true;
+        this.hasChlorophyll = true;
+        this.height = height;
+        this.weight = weight;
+        this.hasRoots = true;
     }
 }
+
+
+
